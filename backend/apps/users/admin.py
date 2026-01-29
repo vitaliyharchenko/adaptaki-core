ca7e127d456cc9d2779f07aef478e16e7fa305f3
vitaliyharchenko <harchenko.grape@gmail.com>
2026-01-29 17:42:49 +0500

    users in django admin site

--- backend/apps/users/admin.py
+++ backend/apps/users/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
 
-# Register your models here.
+User = get_user_model()
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("id", "username", "role", "is_staff", "is_superuser")
+    list_filter = ("role", "is_staff", "is_superuser")
+    search_fields = ("username",)
+    ordering = ("id",)
+
+
+admin.site.unregister(Group)
